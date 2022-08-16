fbd104dd6ee628c4451330259cf3bc5f364ea923
corecaps <corecaps@gmail.com>
2022-08-16 13:58:41 +0200

    talk() now only start handshake sequence

--- client_talk.c
+++ client_talk.c
@@ -14,37 +14,30 @@
 #include "client.h"
 #include "ft_printf/ft_printf.h"
 
-void talk(pid_t pid,char *str)
-{
-	int 	i;
-	char	c;
+/* send Handshake request
+ * TODO : retry handshake until server's ready to take datas
+ **/
 
-	while (str && *str != '\0')
+void talk(pid_t pid)
+{
+	kill(pid,SIGUSR1);
+	/*	while (str && *str != '\0')
 	{
 		c = *str;
 		i = 1;
-//		ft_printf("\n[%d]\n",c);
 
 		while (i < 9)
 		{
-			// binary is transmitted backward !!!!
 			if (c & (1 << (8 - i)))
 			{
-
 				kill(pid, SIGUSR1);
-//				write(1, "\t1", 2);
-
 			}
 			else
-			{
 				kill(pid, SIGUSR2);
-//				write(1, "\t0", 2);
-			}
-			//c = c >> 1;
 			i ++;
 			usleep(500);
 		}
-//		write(1,"\n",1);
 		str ++;
 	}
-}
+*/
+	}
